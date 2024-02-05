710e43ecb2f0e8be95513fdd684e944bf2d01c90
lazuardi <bintanglazuardi70@gmail.com>
2024-02-05 19:12:50 +0700

    make test pass

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,10 +15,10 @@ export default function Login() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="">
+      <div className="max-w-md w-full space-y-8 text-center">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to Forum Apps
+            Sign in to Dicoding Forum Apps
           </h2>
         </div>
         <form className="mt-8 space-y-6" onSubmit={onLogin}>
